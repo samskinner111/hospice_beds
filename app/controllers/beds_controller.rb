44c8e8296efa437d5416f98105a19aca3377f162
freedomevolves <samskinnerphd@gmail.com>
2016-10-12 23:05:39 -0400

    cleanup controllers

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -1,34 +1,26 @@
 class BedsController < ApplicationController
   before_action :set_bed, only: [:show, :edit, :update, :destroy]
 
-  # GET /beds
-  # GET /beds.json
   def index
     @beds = Bed.all
   end
 
-  # GET /beds/1
-  # GET /beds/1.json
   def show
   end
 
-  # GET /beds/new
   def new
     @bed = Bed.new
   end
 
-  # GET /beds/1/edit
   def edit
   end
 
-  # POST /beds
-  # POST /beds.json
   def create
     @bed = Bed.new(bed_params)
 
     respond_to do |format|
       if @bed.save
-        format.html { redirect_to @bed, notice: 'Bed was successfully created.' }
+        format.html { redirect_to @bed, notice: 'Bed was successfully added.' }
         format.json { render :show, status: :created, location: @bed }
       else
         format.html { render :new }
@@ -37,8 +29,6 @@ class BedsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /beds/1
-  # PATCH/PUT /beds/1.json
   def update
     respond_to do |format|
       if @bed.update(bed_params)
@@ -51,18 +41,16 @@ class BedsController < ApplicationController
     end
   end
 
-  # DELETE /beds/1
-  # DELETE /beds/1.json
   def destroy
     @bed.destroy
     respond_to do |format|
-      format.html { redirect_to beds_url, notice: 'Bed was successfully destroyed.' }
+      format.html { redirect_to beds_url, notice: 'Bed was successfully deleted.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_bed
       @bed = Bed.find(params[:id])
     end
--- app/controllers/hospices_controller.rb
+++ app/controllers/hospices_controller.rb
@@ -1,34 +1,26 @@
 class HospicesController < ApplicationController
   before_action :set_hospice, only: [:show, :edit, :update, :destroy]
 
-  # GET /hospices
-  # GET /hospices.json
   def index
     @hospices = Hospice.all
   end
 
-  # GET /hospices/1
-  # GET /hospices/1.json
   def show
   end
 
-  # GET /hospices/new
   def new
     @hospice = Hospice.new
   end
 
-  # GET /hospices/1/edit
   def edit
   end
 
-  # POST /hospices
-  # POST /hospices.json
   def create
     @hospice = Hospice.new(hospice_params)
 
     respond_to do |format|
       if @hospice.save
-        format.html { redirect_to @hospice, notice: 'Hospice was successfully created.' }
+        format.html { redirect_to @hospice, notice: 'Hospice was successfully added.' }
         format.json { render :show, status: :created, location: @hospice }
       else
         format.html { render :new }
@@ -37,8 +29,6 @@ class HospicesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /hospices/1
-  # PATCH/PUT /hospices/1.json
   def update
     respond_to do |format|
       if @hospice.update(hospice_params)
@@ -51,18 +41,16 @@ class HospicesController < ApplicationController
     end
   end
 
-  # DELETE /hospices/1
-  # DELETE /hospices/1.json
   def destroy
     @hospice.destroy
     respond_to do |format|
-      format.html { redirect_to hospices_url, notice: 'Hospice was successfully destroyed.' }
+      format.html { redirect_to hospices_url, notice: 'Hospice was successfully deleted.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_hospice
       @hospice = Hospice.find(params[:id])
     end
