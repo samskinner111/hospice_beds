18814b9b3fd9f414c7515a4b4a1e42f5d6678435
freedomevolves <samskinnerphd@gmail.com>
2016-10-13 07:05:20 -0400

    add console/index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :beds
   resources :hospices
 
+  get 'console/index'
+
   get '/public_pages/about', as: 'about'
   get '/public_pages/contact_us', as: 'contact_us'
   get '/public_pages/legal', as: 'legal'
