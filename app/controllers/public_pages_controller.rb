25f27f476cac82521ab0ebdd7e2c7cd3a54752d0
freedomevolves <samskinnerphd@gmail.com>
2016-10-13 06:02:26 -0400

    add public pages

---
+++ app/controllers/public_pages_controller.rb
@@ -0,0 +1,18 @@
+class PublicPagesController < ApplicationController
+
+  def about
+  end
+
+  def contact_us
+  end
+
+  def home
+  end
+
+  def legal
+  end
+
+  def privacy_policy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :beds
   resources :hospices
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/public_pages/about', as: 'about'
+  get '/public_pages/contact_us', as: 'contact_us'
+  get '/public_pages/legal', as: 'legal'
+  get '/public_pages/privacy_policy', as: 'privacy_policy'
+  
+  root 'public_pages#home'  
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
